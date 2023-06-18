d97f226dfbbb7d9c93a3762030fe2e538d4ed55a
Atulkumar Manchalwar <117819775+DrAtulkumar@users.noreply.github.com>
2023-06-18 09:49:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 function decimalToBinary(decimal) {
-  return decimal.toString(2);
+  let binaryNum = new Array(32);
+ 
+    // counter for binary array
+    let i = 0;
+    while (decimal > 0) {
+         // storing remainder in binary array
+        binaryNum[i] = decimal % 2;
+        decimal = Math.floor(decimal / 2);
+        i++;
+    }
+ return binaryNum;
+    // printing binary array in reverse order
+    // for (let j = i - 1; j >= 0; j--)
+    //     document.write(binaryNum[j]);
 }
 
-// Example usage
-const decimalNumber = 42;
-const binaryNumber = decimalToBinary(decimalNumber);
 
-console.log(binaryNumber);
